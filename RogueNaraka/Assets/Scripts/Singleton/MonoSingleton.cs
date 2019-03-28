f8a59be2460fc2fb1c0a9e22a55a205328162631
ZoNaDak <kbh3719net@gmail.com>
2019-03-28 12:41:17 +0900

    싱글톤 클래스 추가

---
+++ RogueNaraka/Assets/Scripts/Singleton/MonoSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RogueNaraka.Singleton {
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+        protected static T instance;
+        public static T Instance {
+            get {
+                instance = FindObjectOfType (typeof(T)) as T;
+
+                if(instance == null) {
+                    instance = new GameObject("@" + typeof(T).ToString(), typeof(T)).GetComponent<T>();
+                    DontDestroyOnLoad(instance);
+                    Debug.Log(string.Format("Create MonoSingleton Instance! Component Name : {0}", instance.name));
+                }
+
+                return instance;
+            }
+        }
+
+        public virtual void OnDestroy() {
+            Destroy(instance);
+            instance = null;
+            Resources.UnloadUnusedAssets();
+        }
+    }
+}
---
+++ RogueNaraka/Assets/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RogueNaraka.Singleton {
+    public class Singleton<T> where T : class {
+        protected static T instance = null;
+        public static T Instance {
+            get {
+                if(instance == null) {
+                    instance = System.Activator.CreateInstance(typeof(T)) as T;
+                }
+                return instance;
+            }
+        }
+
+        public virtual void Destroy() {
+            instance = null;
+        }
+    }
+}
