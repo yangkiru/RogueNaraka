8e7f1aa51fe2e0a3e36d56e8ab507b6c70ac62a8
ZoNaDak <kbh3719net@gmail.com>
2019-04-09 22:58:19 +0900

    팝업 관련 스크립트 추가중

---
+++ RogueNaraka/Assets/Scripts/PopUp/OneButtonPopUpController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RogueNaraka.PopUpScripts {
+    public class OneButtonPopUpController : MonoBehaviour {
+        public Text context;
+    }
+}
---
+++ RogueNaraka/Assets/Scripts/PopUp/PopUpManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RogueNaraka.SingletonPattern;
+
+namespace RogueNaraka.PopUpScripts {
+    public class PopUpManager : MonoSingleton<PopUpManager> {
+        
+    }
+}
+
