d67f63b900bab31350e17a07c47636e1f58beadc
ZoNaDak <kbh3719net@gmail.com>
2019-04-11 18:58:17 +0900

    텍스트 양에따른 팝업 크기 조절 작업 완료

--- RogueNaraka/Assets/Scripts/PopUp/OneButtonPopUpController.cs
+++ RogueNaraka/Assets/Scripts/PopUp/OneButtonPopUpController.cs
@@ -16,6 +16,7 @@ namespace RogueNaraka.PopUpScripts {
         public void SetPopUpData(string _context, Action<OneButtonPopUpController> _action) {
             this.Context.text = _context;
             this.buttonAction = _action;
+            Debug.LogError(this.Context.rectTransform.sizeDelta);
         }
 
         public void OnClickButton() {
