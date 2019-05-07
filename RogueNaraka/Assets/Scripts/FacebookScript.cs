0ffec49bea41d05d2dd8bd972bca524043798327
Magicboy <kimjuik@magicboy.net>
2019-05-07 21:32:21 +0900

    Facebook init 스크립트 수정

--- RogueNaraka/Assets/Scripts/FacebookScript.cs
+++ RogueNaraka/Assets/Scripts/FacebookScript.cs
@@ -8,7 +8,12 @@ public class FacebookScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FB.Init();
+        FB.Init(this.OnInitComplete);
+        
+    }
+
+    private void OnInitComplete()
+    {
         var tutParams = new Dictionary<string, object>();
         tutParams[AppEventParameterName.ContentID] = "Facebook SDK init";
         tutParams[AppEventParameterName.Description] = "Facebook SDK init!";
@@ -19,7 +24,6 @@ public class FacebookScript : MonoBehaviour
             parameters: tutParams
         );
     }
-
     // Update is called once per frame
     void Update()
     {
