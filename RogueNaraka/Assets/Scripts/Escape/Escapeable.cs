9a21666d39e89ae89b0868717b04fcba63da3c04
yangkiru <skrtmd1124@naver.com>
2019-05-16 04:01:12 +0900

    EscapeManager 수정

---
+++ RogueNaraka/Assets/Scripts/Escape/Escapeable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System;
+using UnityEngine.UI;
+
+public class Escapeable : MonoBehaviour
+{
+    public EscapeEvent onEscape;
+    public void OnEscape()
+    {
+        if (onEscape != null)
+            onEscape.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        EscapeManager.Instance.Stack.Push(this);
+    }
+
+    private void OnDisable()
+    {
+        if (EscapeManager.Instance.Stack.Count != 0)
+            EscapeManager.Instance.Stack.Pop();
+    }
+
+    public void OnClick(Button btn)
+    {
+        if (btn.onClick != null)
+            btn.onClick.Invoke();
+    }
+
+    [Serializable]
+    public class EscapeEvent : UnityEvent
+    {
+    }
+}
--- RogueNaraka/Assets/Scripts/Managers/EscapeManager.cs
+++ RogueNaraka/Assets/Scripts/Managers/EscapeManager.cs
@@ -2,46 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using RogueNaraka.SingletonPattern;
 
 public class EscapeManager : MonoBehaviour
 {
-    public Button pauseBtn;
-    public Button settingBtn;
-#if UNITY_ANDROID
-    
     bool isExit;
+    public static EscapeManager Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Stack<Escapeable> Stack { get { return stack; } }
+    private Stack<Escapeable> stack = new Stack<Escapeable>();
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (DeathManager.instance.deathPnl.gameObject.activeSelf)
-            {
-                if (isExit)
-                    GameManager.instance.ExitGame();
-                else
-                {
-                    isExit = true;
-                    StartCoroutine(ExitCancel(1));
-                }
-            }
-            else
-            {
-                if (pauseBtn.gameObject.activeSelf)
-                    pauseBtn.onClick.Invoke();
-                else if (settingBtn)
-                    settingBtn.onClick.Invoke();
-            }
+            if (stack.Count != 0)
+                stack.Peek().OnEscape();
         }
     }
-
-    IEnumerator ExitCancel(float t)
-    {
-        do
-        {
-            yield return null;
-            t -= Time.unscaledDeltaTime;
-        } while (t > 0);
-        isExit = false;
-    }
-#endif
 }
